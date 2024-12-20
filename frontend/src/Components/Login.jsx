f94970375ed9d970736b6550ea24ad52a5e2892f
sebastian-sultz <hiadarshtripathi@gmail.com>
2024-12-21 04:46:55 +0530

    url update

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -28,7 +28,7 @@ function Login() {
             return handleError('email and password are required')
         }
         try {
-            const url = `http://localhost:8080/auth/login`;
+            const url = `http://35.154.132.104:8080/auth/login`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
